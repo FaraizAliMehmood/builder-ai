df1a2be67f6e64c1de8fbf0a244e7eee485cd91d
FaraizAliMehmood <ibrahimsoft004@gmail.com>
2026-08-15 13:59:01 +0500

    created authenticated forms

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,14 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import api from "../api/api";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 const AppContext = createContext(undefined);
 export function AppContextProvider({children}) {
 
 
+    const navigate = useNavigate();
+
+ 
 // Auth States
 const [user, setUser] = useState(null);
 const [loadingUser, setLoadingUser] = useState(false);
@@ -13,7 +18,7 @@ const [loadingUser, setLoadingUser] = useState(false);
     try {
         setLoadingUser(true);
         const {data} = await api.get("/api/auth/me");
-       // setUser(data.user);
+        setUser(data.user);
     } catch (error) {
         setUser(null);
     } finally {
@@ -21,13 +26,46 @@ const [loadingUser, setLoadingUser] = useState(false);
     }
  }
 
+
+
+
+
+  const login = async (email,password) => {
+    try {
+        const {data} = await api.post("/api/auth/login", {email,password});
+        setUser(data.user);
+        toast.success("Logged in successfully");
+        navigate("/");
+    } catch (err) {
+        console.error("Login failed:", err);
+        const errorMsg = err?.response?.data?.error || "Invalid email and password";
+        toast.error(errorMsg);
+        throw new Error(errorMsg);
+    }
+  }
+
+   const register = async (name,email,password) => {
+    try {
+        const {data} = await api.post("/api/auth/register", {name,email,password});
+        setUser(data.user);
+        toast.success("Registered successfully");
+        navigate("/");
+    } catch (err) {
+        console.error("Registration failed:", err);
+        const errorMsg = err?.response?.data?.error || "Registration failed";
+        toast.error(errorMsg);
+        throw new Error(errorMsg);
+    }
+  }
+  
+
  useEffect(() => {
    checkSession();
  }, [checkSession])
  
 
     return(
-        <AppContext.Provider value={{user,loadingUser}}>
+        <AppContext.Provider value={{user,loadingUser,login,register}}>
         {children}
         </AppContext.Provider>
     )
